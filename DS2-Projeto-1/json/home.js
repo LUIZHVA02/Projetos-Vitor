ba1958b0d9705a1676d0488882da9bdcd5a999d3
Luiz Henrique Vidal Araújo <luizhva02@gmail.com>
2024-02-29 07:34:23 -0300

    07:34-29/02/2024

--- DS2-Projeto-1/json/home.js
+++ DS2-Projeto-1/json/home.js
@@ -2,9 +2,26 @@
 
 const idUser = localStorage.getItem('idUserX')
 
+async function pegarInfo() {
+
+    const url = 'http://localhost:8080/tarefas'
+    const response = await fetch(url)
+    const infoTarefa = await response.json()
+    return infoTarefa
+}
+
 async function mostrarTarefas(){
-    const carouselItem = document.createElement('div')
-    carouselItem.classList.add('carousel-item')
+    const holderInfo = document.getElementById('holderInfo').value
+    const nomeDataTarefa = document.getElementById('labelDataTarefa').value
+    const nomeTarefa = document.createElement('h2')
+    const dataTarefa = document.getElementById('diaTarefa').value
+    const descricaoTarefa = document.getElementById('descTarefa').value
+
+    nomeTarefa.textContent = pegarInfo()
+    
+    
+    nomeDataTarefa.append(nomeTarefa, dataTarefa)
+    holderInfo.append(nomeDataTarefa,descricaoTarefa)
 }
 
 async function cadastrarTarefas() {
